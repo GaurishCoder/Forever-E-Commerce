9261037b35e91ae62ef2aa500f8e2e209f9f392a
Gaurish Naik <dayanand.naik9809@gmail.com>
2025-07-19 00:10:25 +0530

    Updated Server File

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["https://forever-frontend-seven-phi.vercel.app", "http://localhost:5174"],
     credentials: true,
   })
 );
