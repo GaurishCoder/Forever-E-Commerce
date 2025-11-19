a0657a6fc263b466ac8eb2da1767db4686786a7b
Gaurish Naik <dayanand.naik9809@gmail.com>
2025-11-20 01:32:34 +0530

    Change User Controller"

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -22,8 +22,8 @@ const registerUser = async (req, res) => {
     let token = user.generateToken(userPayload);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: false,
-      sameSite: "Lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
     });
     res.status(200).json({ message: "User Created", user: userData , username:name });
